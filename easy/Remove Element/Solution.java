f4d685e79b121ca4b2339be8c6eaf402c115ea1a
최유정 <yj.choi@piolink.com>
2021-11-09 10:34:43 +0900

    #27 refactoring

--- easy/Remove Element/Solution.java
+++ easy/Remove Element/Solution.java
@@ -1,27 +1,14 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int count = 0;
-        if(nums.length == 1){
-            if(nums[0]==val){
-                return 0;
-            }
-            else{
-                return 1;
+        int index = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[index++] = nums[i];
             }
         }
         
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]!=val){
-                nums[count] = nums[i];
-                count ++;
-             }
-        }
-        
-        if(nums.length == count-1){
-            return nums.length;
-        }
-
-        return count;
+        return index;
         
     }
 }
